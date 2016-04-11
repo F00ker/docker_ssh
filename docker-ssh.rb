e359a106bc51844aed48a60f6e3a01cb00745470
sfort <steffy.fort@fr.clara.net>
2016-04-11 16:00:36 +0200

    Fix scp, add rsync option, change conf hiera

--- docker-ssh.rb
+++ docker-ssh.rb
@@ -20,7 +20,7 @@ error   = "[#{red}error#{reset}]"
 
 
 # Check if argv exist
-if ARGV.length != 4
+if ARGV.length != 4 && ARGV.length != 5
   puts "#{error} Error, bad argument please read README"
   Kernel.exit(1)
 end
@@ -30,48 +30,19 @@ end
 curr_ip   = ARGV[0].split(" ").first
 cmd       = ARGV[1]
 user      = ARGV[2]
-home_env  = ARGV[3]
-c_user    = '<container_user>'
+home_user = "#{ARGV[3}docker-ssh#{user}"
 root_dir  = '/etc/docker-ssh'
-uid       = '<unix_uid>'
-gid       = nil
-conf      = "#{root_dir}/conf"
+conf      = "#{root_dir}/extra"
 params    = "#{root_dir}/docker-ssh.passwd"
 d_bashrc  = "#{conf}/bashrc_default"
 d_bashpf  = "#{conf}/bash_profile_default"
 index     = 1
-home_user = "#{home_env}/docker-ssh_home_#{user}"
 
-# Check if user env exist
-if !File.exist?(home_user)
-	FileUtils.mkdir_p home_user, :mode => 0775
-	FileUtils.chown_R uid, gid, home_user
-elsif !File.directory?(home_user)
-	puts "#{error} Home is not a directory."
-	Kernel.exit(1)
-end
-
-if File.exist?(d_bashrc)
-  if !File.exist?("#{home_user}/.bashrc")
-    FileUtils.cp d_bashrc, "#{home_user}/.bashrc"
-    FileUtils.chown uid, gid, "#{home_user}/.bashrc"
-  end
+# Check rsync value
+if ARGV[4] =~ /^--rsync=(unix|rsyncd)$/
+  rsync = $1
 else
-  puts "#{warning} Default bashc doesn\'t exist."
-end
-
-if File.exist?(d_bashpf)
-  if !File.exist?("#{home_user}/.bash_profile")
-    FileUtils.cp d_bashpf, "#{home_user}/.bash_profile"
-    FileUtils.chown uid, gid, "#{home_user}/.bash_profile"
-  end
-else
-  puts "#{warning} Default bash_profile doesn\'t exist."
-end
-
-if !File.exist?(g_bashpf)
-  puts "#{error} Global bash_profile doesn\'t exist. "
-  Kernel.exit(1)
+  rsync = nil
 end
 
 
@@ -92,6 +63,63 @@ File.open(params, 'r') do |in_params|
   end
 end
 
+c_conf  = "#{root_dir}/containers/#{@tag}/properties.conf"
+
+
+# Check if container params fil exist
+if !File.exist?(c_conf)
+  puts "#{error} Params container #{c_conf} doesn't exist"
+  Kenrel.exit(1)
+end
+
+# Recovery container params
+File.open(c_conf, 'r') do |in_conf|
+  in_conf.each_line do |l_conf|
+    if l_conf =~ /unix_uid\p{Blank}*=\p{Blank}*(.*)\p{Blank}*$/
+      @uid  = $1
+    end
+    if l_conf =~ /unix_gid\p{Blank}*=\p{Blank}*(.*)\p{Blank}*$/
+      @gid  = $1
+    end
+    if l_conf =~ /container_user\p{Blank}*=\p{Blank}*(.*)\p{Blank}*$/
+      @c_user = $1
+    end
+  end
+end
+
+
+# Check if user env exist
+if !File.exist?(home_user)
+	FileUtils.mkdir_p home_user, :mode => 0775
+	FileUtils.chown_R @uid, @gid, home_user
+elsif !File.directory?(home_user)
+	puts "#{error} Home is not a directory."
+	Kernel.exit(1)
+end
+
+if File.exist?(d_bashrc)
+  if !File.exist?("#{home_user}/.bashrc")
+    FileUtils.cp d_bashrc, "#{home_user}/.bashrc"
+    FileUtils.chown @uid, @gid, "#{home_user}/.bashrc"
+  end
+else
+  puts "#{warning} Default bashc doesn\'t exist."
+end
+
+if File.exist?(d_bashpf)
+  if !File.exist?("#{home_user}/.bash_profile")
+    FileUtils.cp d_bashpf, "#{home_user}/.bash_profile"
+    FileUtils.chown @uid, @gid, "#{home_user}/.bash_profile"
+  end
+else
+  puts "#{warning} Default bash_profile doesn\'t exist."
+end
+
+if !File.exist?(g_bashpf)
+  puts "#{error} Global bash_profile doesn\'t exist. "
+  Kernel.exit(1)
+end
+
 
 # Check if scp command
 if cmd =~ /^scp( -v)?( -r)?( -d)? -(t|f) (.*)/
@@ -99,14 +127,32 @@ if cmd =~ /^scp( -v)?( -r)?( -d)? -(t|f) (.*)/
 
   @forward_dir.split(',').each do |l_dir|
     l0_dir  = File.join(l_dir, "")
-    if scp_dir =~ /^#{l0_dir}.*/
-      system( "sudo -u \\##{uid} #{cmd}" )
+    if scp_dir =~ /^(#{l0_dir}|#{home_user}).*/
+      system( "sudo -u \\##{@uid} #{cmd}" )
       Kernel.exit(0)
     end
   end
   puts "#{error} #{scp_dir} Permission denied"
   Kernel.exit(1)
+
+elsif cmd =~ /^rsync/
+
+  # If rsync disabled (default value)
+  if rsync.nil?
+    puts "#{error} Rsync disabled. Please contact system administrator"
+    Kernel.exit(1)
+  end
+
+  if rsync == "unix"
+    rsync_cmd = "sudo -u \\##{@uid} #{cmd}"
+  elsif rsync == "rsyncd"
+    rsync_cmd = cmd
+  end
+
+  system( rsync_cmd )
+  Kernel.exit(0)
 end
+
 
 # Show if container already exist
 docker_ps = `docker ps`
