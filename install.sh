589e6aa7f63955dfed064a8e53b992cafa0aa479
sfort <steffy.fort@fr.clara.net>
2016-04-25 21:54:44 +0200

    Fix enter sudoers conf

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ sudoers_config() {
           then
             mkdir -p ${sudoers}.d
           fi
-          printf '%%docker-ssh ALL= NOPASSWD: /usr/local/bin/docker-ssh' > ${sudoers}.d/docker-ssh
+          printf "%%docker-ssh ALL= NOPASSWD: /usr/local/bin/docker-ssh\n" > ${sudoers}.d/docker-ssh
       else
         printf "${error} Sudoers file was not found, please add in your sudoers config : \" %docker-ssh ALL= NOPASSWD: /usr/local/bin/docker-ssh \" \n"
       fi
